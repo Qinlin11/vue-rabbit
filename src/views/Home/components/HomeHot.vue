fea1fad4254c3e877c8b90dbe602d52bc3de4fc8
zhou <1129016236@qq.com>
2023-08-18 21:45:37 +0800

    home的整体结构的拆分

---
+++ src/views/Home/components/HomeHot.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+我是Hot
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home/components/HomeProduct.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+我是product
+</template>
+
+<style scoped>
+
+</style>

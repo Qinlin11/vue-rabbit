8c52745f67108596f65b38cc8f47e3eda97d5d0f
zhou <1129016236@qq.com>
2023-08-19 17:25:35 +0800

    home-product常规渲染

--- src/main.js
+++ src/main.js
@@ -8,31 +8,16 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 
-import { useIntersectionObserver } from '@vueuse/core'
+//懒加载指令插件
+import {lazyPlugin} from "@/directtives";
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
 
 
-//定义全局指令
-app.directive('img-lazy', {
-    mounted(el, binding){
-        //el指令绑定的那个元素
-        //binding：binding.value 指令等于号后面绑定的表达式的值 图片Url
-        console.log(el, binding.value)
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-                console.log(isIntersecting)
-                if(isIntersecting){
-                    //进入视口区域
-                    el.src = binding.value
-                }
-            },
-        )
-    }
-})
+

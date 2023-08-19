dc7d016cc03d18e3f5255422e39ff692e1ff529b
zhou <1129016236@qq.com>
2023-08-19 20:59:29 +0800

    一级分类业务逻辑的拆分

---
+++ src/views/Category/composables/useCategory.js
@@ -0,0 +1,26 @@
+//封装分类数据页面相关代码
+import {getCategory2API} from "@/apis/category";
+import {onMounted, ref} from "vue";
+
+export function useCategory() {
+    //获取数据
+    const categoryDate = ref({})
+    //获取路由参数
+    const route = useRoute()
+    const getCateGory = async (id = route.params.id) => {
+        const res = await getCategory2API(id)
+        categoryDate.value = res.data.result
+    }
+    onMounted(() => getCateGory())
+
+    //目标：希望路由在变化的时候 可以把分类的数据接口重新发送
+
+    onBeforeRouteUpdate((to) => {
+        //存在的问题： 使用最新的路由参数请求最新的分类数据
+        getCateGory(to.params.id)
+    })
+
+    return {
+        categoryDate
+    }
+}
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,35 +1,14 @@
 <script setup>
-import {getCategory2API} from "@/apis/category";
-import {getBannerAPI} from "@/apis/home";
-import {onMounted, ref} from "vue";
 import GoodsItem from "@/views/Home/components/GoodsItem.vue";
-//获取数据
-const categoryDate = ref({})
-//获取路由参数
-const route = useRoute()
-const getCateGory = async (id = route.params.id) => {
-    const res = await getCategory2API(id)
-    categoryDate.value = res.data.result
-}
-onMounted(() => getCateGory())
+import {useBanner} from "@/views/Category/composables/useBanner";
+import {useCategory} from "@/views/Category/composables/useCategory";
 
-//目标：希望路由在变化的时候 可以把分类的数据接口重新发送
+const {bannerList} = useBanner()
+const {categoryDate} = useCategory()
 
-onBeforeRouteUpdate((to) => {
-    //存在的问题： 使用最新的路由参数请求最新的分类数据
-    getCateGory(to.params.id)
-})
 
 //获取banner
-const bannerList = ref([])
-const getBanner = async () => {
-    const res = await getBannerAPI({
-        distributionSite:'2'
-    })
-    bannerList.value = res.data.result
 
-}
-onMounted(() => getBanner())
 </script>
 
 <template>
@@ -39,7 +18,7 @@ onMounted(() => getBanner())
             <div class="bread-container">
                 <el-breadcrumb separator=">">
                     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                    <el-breadcrumb-item>{{categoryDate.name}}</el-breadcrumb-item>
+                    <el-breadcrumb-item>{{ categoryDate.name }}</el-breadcrumb-item>
                 </el-breadcrumb>
             </div>
             <!--轮播图-->
@@ -55,7 +34,7 @@ onMounted(() => getBanner())
                 <ul>
                     <li v-for="i in categoryDate.children" :key="i.id">
                         <RouterLink to="/">
-                            <img v-img-lazy="i.picture" />
+                            <img v-img-lazy="i.picture"/>
                             <p>{{ i.name }}</p>
                         </RouterLink>
                     </li>
@@ -66,7 +45,7 @@ onMounted(() => getBanner())
                     <h3>- {{ item.name }}-</h3>
                 </div>
                 <div class="body">
-                    <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+                    <GoodsItem v-for="good in item.goods" :good="good" :key="good.id"/>
                 </div>
             </div>
         </div>
@@ -76,91 +55,92 @@ onMounted(() => getBanner())
 
 <style scoped lang="scss">
 .top-category {
-    h3 {
-        font-size: 28px;
-        color: #666;
-        font-weight: normal;
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+
+      li {
+        width: 168px;
+        height: 160px;
+
+
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+
+          img {
+            width: 100px;
+            height: 100px;
+          }
+
+          p {
+            line-height: 40px;
+          }
+
+          &:hover {
+            color: $xtxColor;
+          }
+        }
+      }
+    }
+  }
+
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+
+      .tag {
         text-align: center;
-        line-height: 100px;
-    }
-
-    .sub-list {
-        margin-top: 20px;
-        background-color: #fff;
-
-        ul {
-            display: flex;
-            padding: 0 32px;
-            flex-wrap: wrap;
-
-            li {
-                width: 168px;
-                height: 160px;
-
-
-                a {
-                    text-align: center;
-                    display: block;
-                    font-size: 16px;
-
-                    img {
-                        width: 100px;
-                        height: 100px;
-                    }
-
-                    p {
-                        line-height: 40px;
-                    }
-
-                    &:hover {
-                        color: $xtxColor;
-                    }
-                }
-            }
-        }
-    }
-
-    .ref-goods {
-        background-color: #fff;
-        margin-top: 20px;
+        color: #999;
+        font-size: 20px;
         position: relative;
-
-        .head {
-            .xtx-more {
-                position: absolute;
-                top: 20px;
-                right: 20px;
-            }
-
-            .tag {
-                text-align: center;
-                color: #999;
-                font-size: 20px;
-                position: relative;
-                top: -20px;
-            }
-        }
-
-        .body {
-            display: flex;
-            justify-content: space-around;
-            padding: 0 40px 30px;
-        }
+        top: -20px;
+      }
     }
 
-    .bread-container {
-        padding: 25px 0;
+    .body {
+      display: flex;
+      justify-content: space-around;
+      padding: 0 40px 30px;
     }
+  }
+
+  .bread-container {
+    padding: 25px 0;
+  }
 }
+
 .home-banner {
-    width: 1240px;
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+
+  img {
+    width: 100%;
     height: 500px;
-    margin: 0 auto;
-
-
-    img {
-        width: 100%;
-        height: 500px;
-    }
+  }
 }
 </style>

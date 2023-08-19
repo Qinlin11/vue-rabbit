6ab7190d3a75b3aaa57a6abcd08786d3ab3fd5b7
zhou <1129016236@qq.com>
2023-08-19 20:14:03 +0800

    home 轮播图的实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {getCategory2API} from "@/apis/category";
-
+import {getBannerAPI} from "@/apis/home";
+import {onMounted, ref} from "vue";
 //获取数据
 const categoryDate = ref({})
 //获取路由参数
@@ -10,6 +11,17 @@ const getCateGory = async () => {
     categoryDate.value = res.data.result
 }
 onMounted(() => getCateGory())
+
+//获取banner
+const bannerList = ref([])
+const getBanner = async () => {
+    const res = await getBannerAPI({
+        distributionSite:'2'
+    })
+    bannerList.value = res.data.result
+
+}
+onMounted(() => getBanner())
 </script>
 
 <template>
@@ -21,6 +33,14 @@ onMounted(() => getCateGory())
                     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
                     <el-breadcrumb-item>{{categoryDate.name}}</el-breadcrumb-item>
                 </el-breadcrumb>
+            </div>
+            <!--轮播图-->
+            <div class="home-banner">
+                <el-carousel height="500px">
+                    <el-carousel-item v-for="item in bannerList" :key="item.id">
+                        <img :src="item.imgUrl" alt="">
+                    </el-carousel-item>
+                </el-carousel>
             </div>
         </div>
     </div>
@@ -103,6 +123,17 @@ onMounted(() => getCateGory())
 
     .bread-container {
         padding: 25px 0;
+    }
+}
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    margin: 0 auto;
+
+
+    img {
+        width: 100%;
+        height: 500px;
     }
 }
 </style>

4637fb4338005aad2de3002d50af6e7a0d03cb49
zhou <1129016236@qq.com>
2023-08-21 19:09:56 +0800

    登录-退出登录业务实现

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,7 +1,18 @@
 <script setup>
 import {userUserStore} from "@/stores/user";
-const userStore = userUserStore()
+import {useRouter} from "vue-router";
 
+const userStore = userUserStore()
+const router = useRouter()
+const confirm = () => {
+    console.log('退出登录')
+    //退出业务实现
+    //1.清除用户信息 触发action
+    userStore.clearUserInfo()
+    //2.跳转登录页
+    router.push('/login')
+
+}
 </script>
 
 <template>
@@ -12,7 +23,7 @@ const userStore = userUserStore()
                 <template v-if="userStore.userInfo.token">
                     <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
                     <li>
-                        <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+                        <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
                             <template #reference>
                                 <a href="javascript:;">退出登录</a>
                             </template>

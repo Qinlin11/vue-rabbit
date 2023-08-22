8a23e94451508a56338a7fce7e880bcff58416fa
zhou <1129016236@qq.com>
2023-08-22 18:11:37 +0800

    本地购物车-列表购物车-全选功能

--- src/stores/carStore.js
+++ src/stores/carStore.js
@@ -35,18 +35,29 @@ export const useCartStore = defineStore('cart', () => {
         const item = cartList.value.find(item => item.skuId === skuId)
         item.selected = selected
     }
+    //全选功能
+    const allCheck = (selected) => {
+        //把cartList中的每一项的selected都设置为当前的全选框状态
+        cartList.value.forEach(item => item.selected = selected)
+    }
 
     //计算属性
     //1. 总数量 所有项的count之和
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
+    //是否全选
+    const isALL = computed(() => cartList.value.every(item => item.selected))
+
     return {
         cartList,
         addCart,
         delCart,
+        isALL,
         allCount,
         allPrice,
-        singleCheck
+        singleCheck,
+        allCheck
     }
 
 },{
